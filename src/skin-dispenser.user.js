a3dcde5d7a1d21e18bf71b98f942651d8d8268f4
Saa Saviæ <sasa.savic@protonmail.com>
2016-04-27 20:55:49 +0200

    Make it usable without reloading the browser

--- src/skin-dispenser.user.js
+++ src/skin-dispenser.user.js
@@ -4,19 +4,20 @@
 // @author Saša Savić <sasa.savic@protonmail.com>
 // @description Accepts all empty trade offers (that make you money).
 // @version 1.3.0
-// @include https://steamcommunity.com/id/*/tradeoffers/
-// @include https://steamcommunity.com/tradeoffers
+// @include https://steamcommunity.com/id/*/tradeoffers/?dispenser
+// @include https://steamcommunity.com/tradeoffer*
 // @include https://steamcommunity.com/tradeoffer/*
-// @include http://steamcommunity.com/id/*/tradeoffers/
+// @include http://steamcommunity.com/id/*/tradeoffers/?dispenser
 // @grant window.close()
 // ==/UserScript==
 
 console.log("Starting Skin Dispenser.");
 
 // Constants
-var checkTradesDelay = 1000 * 2; // miliseconds to recheck trades
+var checkTradesDelay = 1000 * 1; // miliseconds to recheck trades
 var checkTradeDelay  = 1000 * 2; // miliseconds to recheck if the trade is loaded
 var clickAcceptDelay = 1000 * 1; // miliseconds to wait to click the accept button after clicking the confirm button
+var loggedInUser = "SaleOne";
 
 // Extend array so it can search for values contained in itself
 Array.prototype.contains = function (value) {
@@ -29,45 +30,25 @@ Array.prototype.contains = function (value) {
 };
 
 // Tracked URLs
-var tradeOffersRegex  = "http(s)?\:\/\/steamcommunity\.com\/id\/[a-zA-Z0-9]+\/tradeoffers[\/]?";
+var tradeOffersRegex  = "http(s)?:\/\/steamcommunity\.com\/id\/[a-zA-Z0-9]+\/tradeoffers\/?\\?dispenser";
 var tradeOfferRegex   = "http(s)?\:\/\/steamcommunity\.com\/tradeoffer\/[0-9]+[\/]?";
 var tradeReceiptRegex = "http(s)?\:\/\/steamcommunity\.com\/trade\/[0-9]+\/receipt";
 
 var pageUrl = window.location.href;
 
+// How many trades have been already checked.
+var tradesChecked = 0;
+
 // Execute only if on wanted page
 if (pageUrl.match(tradeOffersRegex)) {
         console.log("Skin Dispenser: on trade offers page.");
-
-        // How many trades have been already checked.
-        var tradesChecked = parseInt(window.name) || 0;
-
         setInterval(function () {
-            // Get the list of all the trades.
-            var tradeOffers = document.querySelectorAll(".tradeoffer");
-            for (var i = 0; i < (tradeOffers.length - tradesChecked); i++) {
-                // Check all the trade offers to find empty ones.
-                var tradeOffer  = tradeOffers[i];
-                var itemsToGive = tradeOffer.querySelectorAll(".secondary>div.tradeoffer_item_list>div.trade_item");
-                var itemsToGet  = tradeOffer.querySelectorAll(".primary>div.tradeoffer_item_list>div.trade_item");
-                if (itemsToGive.length === 0) {
-                    // Check if the trade is bugged (error 28)
-                    if (!itemsToGet[0].getAttribute("style") ||
-                        itemsToGet[0].classList.contains("missing")) {
-                        // Skip the items if its bugged (described in #1).
-                        continue;
-                    } else {
-                        // We found some free skins. Let's get them.
-                        tradeOffer.querySelector(".link_overlay").click();
-                    }
-                }
-            }
-            // We checked all the trades, store this.
-            window.name = tradeOffers.length.toString();
-
-            // Reload the page to find new trades.
-            location.reload();
+            var httpRequest = new XMLHttpRequest();
+            httpRequest.addEventListener("load", checkTrades);
+            httpRequest.open("GET", "/id/" + loggedInUser + "/tradeoffers/", true); 
+            httpRequest.send();
         }, checkTradesDelay);
+
 } else if (pageUrl.match(tradeOfferRegex)) {
     console.log("Skin Dispenser: on specific trade page.");
     // Get all the items in the trade.
@@ -92,6 +73,39 @@ if (pageUrl.match(tradeOffersRegex)) {
 } else if (pageUrl.match(tradeReceiptRegex)) {
     // This has a chance to close all recipts pages, but it shouldn't (because the script can close only windows it opened).
     window.close();
+}
+
+function checkTrades() {
+    var htmlParser = new DOMParser();
+    var doc = htmlParser.parseFromString(this.responseText, "text/html");
+
+    // Get the list of all the trades.
+    var tradeOffers = doc.querySelectorAll(".tradeoffer");
+    console.log(tradesChecked);
+    for (var i = 0; i < (tradeOffers.length - tradesChecked); i++) {
+
+        // Check all the trade offers to find empty ones.
+        var tradeOffer  = tradeOffers[i];
+        var itemsToGive = tradeOffer.querySelectorAll(".secondary>div.tradeoffer_item_list>div.trade_item");
+        var itemsToGet  = tradeOffer.querySelectorAll(".primary>div.tradeoffer_item_list>div.trade_item");
+        if (itemsToGive.length === 0) {
+            // Check if the trade is bugged (error 28)
+            if (!itemsToGet[0].getAttribute("style") ||
+                itemsToGet[0].classList.contains("missing")) {
+                // Skip the items if its bugged (described in #1).
+
+                tradesChecked += 1;
+                continue;
+            } else {
+                // We found some free skins. Let's get them.
+                var tradeId = tradeOffer.getAttribute("id").split("_")[1];
+                console.log("Opening trade with ID: " + tradeId);
+                window.open("https://steamcommunity.com/tradeoffer/" + tradeId);
+            }
+        }
+
+        tradesChecked += 1;
+    }
 }
 
 function sleep(ms)
