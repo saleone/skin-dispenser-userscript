587a7c0be7be60992d20db24b4b8059d0122d25b
Saša Savić <savic-sasa@outlook.com>
2016-12-25 20:48:54 +0100

    Dont block the script execution

--- src/skin-dispenser.user.js
+++ src/skin-dispenser.user.js
@@ -14,7 +14,7 @@ console.log("Starting Skin Dispenser.");
 
 // Constants
 var checkTradesDelay = 1000 * 1; // miliseconds to recheck trades
-var checkTradeDelay  = 1000 * 2; // miliseconds to recheck if the trade is loaded
+var checkTradeDelay = 1000 * 2; // miliseconds to recheck if the trade is loaded
 var clickAcceptDelay = 1000 * 1; // miliseconds to wait to click the accept button after clicking the confirm button
 
 // Extend array so it can search for values contained in itself
@@ -28,61 +28,66 @@ Array.prototype.contains = function (value) {
 };
 
 // Tracked URLs
-var tradeOffersRegexActive  = "https?\:\/\/steamcommunity\.com\/id\/[a-zA-Z0-9]+\/tradeoffers\/?\\?dispenser";
-var tradeOffersRegex        = "https?\:\/\/steamcommunity\.com\/id\/[a-zA-Z0-9]+\/tradeoffers\/?";
-var tradeOfferRegex         = "https?\:\/\/steamcommunity\.com\/tradeoffer\/[0-9]+[\/]?";
-var tradeReceiptRegex       = "https?\:\/\/steamcommunity\.com\/trade\/[0-9]+\/receipt";
+var tradeOffersRegexActive = "https?\:\/\/steamcommunity\.com\/id\/[a-zA-Z0-9]+\/tradeoffers\/?\\?dispenser";
+var tradeOffersRegex = "https?\:\/\/steamcommunity\.com\/id\/[a-zA-Z0-9]+\/tradeoffers\/?";
+var tradeOfferRegex = "https?\:\/\/steamcommunity\.com\/tradeoffer\/[0-9]+[\/]?";
+var tradeReceiptRegex = "https?\:\/\/steamcommunity\.com\/trade\/[0-9]+\/receipt";
 
 var pageUrl = window.location.href;
 
 // How many trades have been already checked.
 var tradesChecked = 0;
 
-
-
 // Execute only if on wanted page
 if (pageUrl.match(tradeOffersRegex)) {
-    var  dispenseUrl = pageUrl + "?dispenser"
+    var dispenseUrl = pageUrl + "?dispenser"
     aDispense = document.createElement("a");
     aDispense.id = "dispense-button";
     aDispense.className = "btn_darkblue_white_innerfade btn_medium new_trade_offer_btn responsive_OnClickDismissMenu";
     aDispense.innerHTML = "Start dispenser";
-    aDispense.style="margin-top: 12px";
+    aDispense.style = "margin-top: 12px";
     aDispense.href = dispenseUrl;
-    aDispense.addEventListener("click", function() {
+    aDispense.addEventListener("click", function () {
         window.location.href = dispenseUrl;
     });
     document.querySelector(".rightcol_controls_content").appendChild(aDispense);
 
 } else if (pageUrl.match(tradeOffersRegexActive)) {
-        console.log("Skin Dispenser (active): on trade offers page.");
-        setInterval(function () {
-            var httpRequest = new XMLHttpRequest();
-            httpRequest.addEventListener("load", checkTrades);
-            httpRequest.open("GET", window.location.href, true);
-            httpRequest.send();
-        }, checkTradesDelay);
+    console.log("Skin Dispenser (active): on trade offers page.");
+    setInterval(function () {
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.addEventListener("load", checkTrades);
+        httpRequest.open("GET", window.location.href, true);
+        httpRequest.send();
+    }, checkTradesDelay);
 
 } else if (pageUrl.match(tradeOfferRegex)) {
     console.log("Skin Dispenser: on specific trade page.");
     // Get all the items in the trade.
     if (document.querySelector("#error_msg")) {
-         // If there is not offer, close the window.
-         window.close();
+        // If there is not offer, close the window.
+        window.close();
     }
     setInterval(function () {
         var yourItems = document.querySelectorAll("#trade_yours>div.trade_item_box>div#your_slots>div.has_item");
         if (yourItems.length === 0) {
-           document.querySelector("#you_notready").click();
-           sleep(clickAcceptDelay);
-           var reportedButton = document.querySelector("body>div.newmodal>div.newmodal_content_border>div>div.newmodal_buttons>div.btn_green_white_innerfade.btn_medium");
-           if (reportedButton) {
-               // If sender is reported, we don't care. Get 'em all.
-               reportedButton.click();
-               sleep(clickAcceptDelay);
-           }
-           document.querySelector("#trade_confirmbtn").click();
-    }}, checkTradeDelay);
+            document.querySelector("#you_notready").click();
+            // Buttons are dynamicaly generated so we need to wait for them
+            // to get them appear in the DOM.
+            setTimeout(function () {
+                var reportedButton = document.querySelector("body>div.newmodal>div.newmodal_content_border>div>div.newmodal_buttons>div.btn_green_white_innerfade.btn_medium");
+                if (reportedButton) {
+                    // If sender is reported, we don't care. Get 'em all.
+                    reportedButton.click();
+                    setTimeout(function () {
+                        document.querySelector("#trade_confirmbtn").click();
+                    }, clickAcceptDelay);
+                } else {
+                    document.querySelector("#trade_confirmbtn").click();
+                }
+            }, clickAcceptDelay);
+        }
+    }, checkTradeDelay);
 } else if (pageUrl.match(tradeReceiptRegex)) {
     // This has a chance to close all recipts pages, but it shouldn't (because the script can close only windows it opened).
     window.close();
@@ -95,11 +100,10 @@ function checkTrades() {
     // Get the list of all the trades.
     var tradeOffers = doc.querySelectorAll(".tradeoffer");
     for (var i = 0; i < (tradeOffers.length - tradesChecked); i++) {
-
         // Check all the trade offers to find empty ones.
-        var tradeOffer  = tradeOffers[i];
+        var tradeOffer = tradeOffers[i];
         var itemsToGive = tradeOffer.querySelectorAll(".secondary>div.tradeoffer_item_list>div.trade_item");
-        var itemsToGet  = tradeOffer.querySelectorAll(".primary>div.tradeoffer_item_list>div.trade_item");
+        var itemsToGet = tradeOffer.querySelectorAll(".primary>div.tradeoffer_item_list>div.trade_item");
         if (itemsToGive.length === 0) {
             // Check if the trade is bugged (error 28)
             if (!itemsToGet[0].getAttribute("style") ||
@@ -116,12 +120,4 @@ function checkTrades() {
         }
         tradesChecked += 1;
     }
-}
-
-function sleep(ms)
-{
-    // Stop the execution of the script for specified time.
-    var dt = new Date();
-    dt.setTime(dt.getTime() + ms);
-    while (new Date().getTime() < dt.getTime());
 }
